cb25a4e28df08c6a47cc03625a8793dfb917466d
Rapha445 <raphaelle.delmas@lilo.org>
2023-02-20 12:16:43 +0000

    last build

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,8 @@ const postCSSPlugins = [
 class RunAfterCompile {
     apply(compiler) {
         compiler.hooks.done.tap('Copy images', function () {
-            fse.copySync('./app/assets/images', './dist/assets/images')
-        })  // change to docs for GitHub
+            fse.copySync('./app/assets/images', './docs/assets/images')
+        })  // change to docs for GitHub / dist for other
     }
 }
 
@@ -84,8 +84,8 @@ if (currentTask == 'build') {
     config.output = {
         filename: '[name].[chunkhash].js',
         chunkFilename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'dist')
-        // change to docs for GitHub
+        path: path.resolve(__dirname, 'docs')
+        // change to docs for GitHub / dist for other
     }
     config.mode = 'production'
     config.optimization = {
